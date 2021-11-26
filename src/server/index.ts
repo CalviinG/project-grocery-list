42248f91f74f288fa0d65018bcc5df1f35d60270
Emma-Johansson <56669356+Emma-Johansson@users.noreply.github.com>
2021-11-26 11:09:47 +0100

    Add functionality to create list

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,10 +8,11 @@ app.use(express.json());
 app.get('/groceries', groceries.fetchGroceries);
 app.get('/groceries/:id', groceries.fetchGrocery);
 app.delete('/groceries/:id', groceries.removeGrocery);
-app.post('/groceries/:id', groceries.createGrocery);
+app.post('/groceries', groceries.createGrocery);
 app.patch('/groceries/:id', groceries.updateGrocery);
 
 app.get('/lists', lists.fetchLists);
 app.get('/lists/:id', lists.fetchList);
+app.post('/lists', lists.createList);
 
 app.listen(3001);
